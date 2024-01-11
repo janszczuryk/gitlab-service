08038d262980de69c1162c1998906f3c9884ea0d
Jan Szczuryk <jan.szczuryk@gmail.com>
2024-01-11 15:48:50 +0100

    Add base of the project

---
+++ scripts/generate-root-password.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function get_random_password()
+{
+    local password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8)
+    echo $password
+}
+
+# $1 - key
+# $2 - value
+# $3 - .env file path
+function set_env_variable()
+{
+    [ -z "$1" ] && (echo "Need key param"; exit 1);
+    [ -z "$2" ] && (echo "Need value param"; exit 1);
+    [ -z "$3" ] && (echo "Need .env file path param"; exit 1);
+    sed -i "/^$1=/s/=.*/=$2/" "$3"
+}
+
+function main()
+{
+    local password=$(get_random_password)
+    set_env_variable "INITIAL_ROOT_PASSWORD" "$password" ".env"
+    echo "Generated root password is: $password"
+}
+
+main
